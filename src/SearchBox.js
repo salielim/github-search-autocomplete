875e99552193267bb1c3e8595212a566c61204b2
Salie Lim <salie.lim@gmail.com>
2017-10-03 10:29:14 +0800

    Add basic API call for repo

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://api.github.com/search/repositories?q=react';
+
+    axios
+      .get(url)
+      // fake server, see https://github.com/salielim/doc-fake-server
+      .then(({ data }) => {
+        this.setState({
+          data: data.items
+        });
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(
+          'Unable to access data. \n' + err
+        );
+      });
+  }
+
   render() {
+    const child = this.state.data.map((el, index) => {
+      return (
+        <div key={index}>
+          <p>
+            {el.full_name}
+          </p>
+        </div>
+      )
+    });
+
     return (
       <div>
         <input type="text" name="search" placeholder="Search.." />
+
+        <div className="cards-container">
+          {child}
+        </div>
       </div>
-    );
+    )
   }
 }
 
